d57908a54dc3123bd63be9e7ef60a85ae1c1693d
Stephanie <soshiyo@uw.edu>
2020-05-24 21:50:38 -0700

    updated data frame access

--- Midpoint Deliverable/CAT_data.R
+++ Midpoint Deliverable/CAT_data.R
@@ -10,11 +10,11 @@ library(xlsx)
 
 # This is the Climate Action Tracker (CAT) file
 
-# For getting accessing the dataframe, choose the final-project folder as the 
-# working directory!
+# I made it so you don't have to change your working directories to access the
+# data, so you can stay in this file's directory when working in it.
 
 # Get dataframe
-cat_df <- read_excel("data/CAT-Decarbonisation-Indicators.AllData.260919.xlsx",
+cat_df <- read_excel("../data/CAT-Decarbonisation-Indicators.AllData.260919.xlsx",
                      sheet = "RawData")
 
 # currently working to remove the na values from the dataframe, but am 
