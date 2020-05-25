ead3f95cc17c54c82795c0df992aa6d8aaa6db1e
Dana <danarin@uw.edu>
2020-05-24 20:20:32 -0700

    Cat dataframe added

--- Midpoint Deliverable/CAT_data.R
+++ Midpoint Deliverable/CAT_data.R
@@ -4,4 +4,18 @@ library(styler)
 library(stringr)
 library(ggplot2)
 library(leaflet)
-library(knitr)
+library(readxl)
+library(xlsx)
+
+
+# This is the Climate Action Tracker (CAT) file
+
+# For getting accessing the dataframe, choose the final-project folder as the 
+# working directory!
+
+# Get dataframe
+cat_df <- read_excel("data/CAT-Decarbonisation-Indicators.AllData.260919.xlsx",
+                     sheet = "RawData")
+
+# currently working to remove the na values from the dataframe, but am 
+# having trouble.
