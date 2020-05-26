132fe70dfd6bc08f07dca365b63993c0dbce414e
Stephanie <soshiyo@uw.edu>
2020-05-25 20:22:57 -0700

    created map for change in temperatures in countries

--- Midpoint Deliverable/temperature-chart.R
+++ Midpoint Deliverable/temperature-chart.R
@@ -1,8 +1,39 @@
-library(leaflet)
 library(dplyr)
+library(ggplot2)
+require(maps)
 
-temperature_df <- read.csv("../data/GlobalLandTemperaturesByCountry.csv", stringsAsFactors = FALSE)
+temperature_df <- read.csv("../data/GlobalLandTemperaturesByCountry.csv",
+                           stringsAsFactors = FALSE)
 
+temperature_df <- na.omit(temperature_df)
 
+analysis <- temperature_df %>%
+  group_by(Country) %>%
+  filter(dt %in% range(dt, na.rm = TRUE)) %>%
+  summarize(
+    change_in_temp = AverageTemperature[2] - AverageTemperature[1]
+  ) %>%
+  select(Country, change_in_temp) %>%
+  rename(region = Country, Change_in_Temperature = change_in_temp) %>%
+  mutate(
+    region = ifelse(region == "United States", "USA", region),
+    region = ifelse(region == "Congo (Democratic Republic Of The)",
+                    "Democratic Republic of the Congo", region)
+  )
 
+world_map <- map_data("world")
 
+change_temp_map <- left_join(analysis, world_map, by = "region")
+
+map <- ggplot(
+  change_temp_map,
+  aes(map_id = region, fill = Change_in_Temperature)) +
+  geom_map(map = change_temp_map, color = "white") +
+  expand_limits(x = change_temp_map$long, y = change_temp_map$lat) +
+  scale_fill_viridis_c(option = "C")
+
+map <- map + labs(
+  title = "Change in Temperature in Countries",
+  x = "",
+  y = ""
+)
