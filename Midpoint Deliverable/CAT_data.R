8d7266e3d5a90112dc368305e32116f2e0ab99d0
Dana <danarin@uw.edu>
2020-05-26 20:05:32 -0700

    Waste generation added to index

--- Midpoint Deliverable/CAT_data.R
+++ Midpoint Deliverable/CAT_data.R
@@ -1,21 +1,19 @@
 library(dplyr)
 library(ggplot2)
 library(readxl)
-
+library(ggiraph)
 
 
 # This is the Climate Action Tracker (CAT) file
-
-# I made it so you don't have to change your working directories to access the
-# data, so you can stay in this file's directory when working in it.
+# Used to calculate average waste generation per capita
 
 # Get dataframe
 cat_df <- read_excel("../data/CAT-Decarbonisation-Indicators.AllData.260919.xlsx",
                      sheet = "RawData")
 
-# Create a new dataframe grouping by indicators
+# Create a new dataframe grouping by country and filtering by waste
 updated_df <- cat_df %>%
-  select(Indicator, Country, Value) %>% 
+  select(Indicator, Country, Value) %>%
   filter(Indicator == "Waste generation (per capita)") %>%
   group_by(Country) %>%
   summarize(
@@ -23,5 +21,10 @@ updated_df <- cat_df %>%
   )
 
 # Making dataframe into a bar chart
-p <- ggplot(updated_df, mapping = aes(fill = Country, x = Country, y = Average_Waste_Generation)) + geom_bar(position = "stack", stat = "identity")
-p
+p <- ggplot(updated_df, mapping = aes(fill = Country, x = Country,
+                                      y = Average_Waste_Generation))
+p <- p + geom_text(aes(label = round(Average_Waste_Generation, digits = 2)),
+                   vjust = -0.5, position = position_dodge(0.9), size = 2.75) 
+p <- p +  geom_bar(position = "stack",
+                   stat = "identity")
+p <- p + ylab("Average Waste Generation (Per Capita)") 
