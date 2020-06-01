5ada1e0c6e03f78a860d67d8c09161879caa7af6
Stephanie <soshiyo@uw.edu>
2020-06-01 03:55:40 -0700

    created function to make map

--- Final Deliverable/temperature_map.R
+++ Final Deliverable/temperature_map.R
@@ -3,6 +3,8 @@ source("../Midpoint Deliverable/temperature-chart.R")
 
 geography_df <- read.csv("https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv")
 
+geography_df <- geography_df[-2]
+
 temperature_df <- temperature_df[-3]
 
 temperature_df <- temperature_df %>%
@@ -26,8 +28,19 @@ build_map <- function(year_choice) {
                       "Democratic Republic of the Congo", region)
     )
   
+  rename_df <- geography_df %>%
+    rename(region = COUNTRY)
   
+  joined_df <- left_join(calculation, rename_df, by = "region")
+  
+  create_map <- plot_ly(joined_df,
+                        type = "choropleth",
+                        locations = joined_df$CODE,
+                        z = joined_df$Change_in_Temperature,
+                        text = joined_df$region,
+                        colorscale = "BLUES")
+  
+  return(create_map)
 }
-
 
 
