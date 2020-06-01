1ed7dd193e2ea0c475e939d1723ed85fa8d2c3da
Stephanie <soshiyo@uw.edu>
2020-06-01 15:09:07 -0700

    added temperature map output

--- Final Deliverable/my_server.R
+++ Final Deliverable/my_server.R
@@ -1,2 +1,7 @@
+library(shiny)
+source("temperature_map.R")
+
 server <- function(input, output, session) {
+  # Temperature Change Map
+  output$temperature_map <- renderPlotly(build_map(input$year))
 }
