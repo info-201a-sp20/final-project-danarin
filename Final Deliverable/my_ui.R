27dc1303ffd19c6ae6533faa10af6e8675ac0e20
Stephanie <soshiyo@uw.edu>
2020-06-01 15:08:35 -0700

    added temperature tab and setup

--- Final Deliverable/my_ui.R
+++ Final Deliverable/my_ui.R
@@ -1,1 +1,43 @@
-ui <- fluidPage()
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel(""),
+  tabsetPanel(
+    # Intro tab
+    temperature_tab
+    # chart 2 tab
+    # chart 3 tab
+    # summary tab
+  )
+)
+
+temperature_tab <- tabPanel(
+  "Temperature Change Map",
+  fluid = TRUE,
+  sidebarLayout(
+    sidebarPanel(
+      h4("TEMPERATURE CHANGE BY YEAR"),
+      p(""),
+      sliderInput(
+        inputId = "year",
+        label = "Year",
+        min = 1743,
+        max = 2013,
+        value = 1878
+      ),
+      h4("QUESTIONS"),
+      p("- Which part of the world is being affected by global
+        warming the most?"),
+      h4("RESULTS"),
+      p("")
+    ),
+    
+    mainPanel(
+      plotlyOutput(
+        outputId = "temperature_map",
+        height = 600,
+        width = 900
+      )
+    )
+  )
+)
