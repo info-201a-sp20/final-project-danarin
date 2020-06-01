9f84227d586c443b970c6774ecb0372061a8af03
Stephanie <soshiyo@uw.edu>
2020-06-01 14:21:38 -0700

    edited styling

--- Final Deliverable/temperature_map.R
+++ Final Deliverable/temperature_map.R
@@ -1,7 +1,13 @@
 library(plotly)
 source("../Midpoint Deliverable/temperature-chart.R")
 
-geography_df <- read.csv("https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv")
+geography_df <- read.csv(
+  paste0(
+    "https://raw.githubusercontent.com/plotly",
+    "/datasets/master",
+    "/2014_world_gdp_with_codes.csv"
+    ),
+  stringsAsFactors = FALSE)
 
 geography_df <- geography_df[-2]
 
@@ -13,7 +19,7 @@ temperature_df <- temperature_df %>%
 build_map <- function(year_choice) {
   select_year <- temperature_df %>%
     filter(year == year_choice)
-  
+
   calculation <- select_year %>%
     group_by(Country) %>%
     filter(dt %in% range(dt, na.rm = TRUE)) %>%
@@ -25,20 +31,23 @@ build_map <- function(year_choice) {
     mutate(
       region = ifelse(region == "USA", "United States", region),
       region = ifelse(region == "Democratic Republic of the Congo",
-                      "Congo (Democratic Republic of the)", region)
+        "Congo (Democratic Republic of the)", region
+      )
     )
-  
+
   rename_df <- geography_df %>%
     rename(region = COUNTRY)
-  
+
   joined_df <- left_join(calculation, rename_df, by = "region")
-  
-  create_map <- plot_ly(joined_df,
-                        type = "choropleth",
-                        locations = joined_df$CODE,
-                        z = joined_df$Change_in_Temperature,
-                        text = joined_df$region,
-                        colorscale = "BLUES")
-  
-  return(create_map)
+
+  temperature_map <- plot_ly(
+    joined_df,
+    type = "choropleth",
+    locations = joined_df$CODE,
+    z = joined_df$Change_in_Temperature,
+    text = joined_df$region,
+    colorscale = "BLUES"
+  )
+
+  return(temperature_map)
 }
