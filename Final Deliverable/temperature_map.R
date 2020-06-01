a1b6668403d752715e63676f87edd4c7ca76986c
Stephanie <soshiyo@uw.edu>
2020-06-01 03:00:51 -0700

    started creating map for temperature

---
+++ Final Deliverable/temperature_map.R
@@ -0,0 +1,34 @@
+library(dplyr)
+library(ggplot2)
+library(maps)
+library(rgdal)
+source("../Midpoint Deliverable/temperature-chart.R")
+
+temperature_df <- temperature_df[-3]
+
+temperature_df <- temperature_df %>%
+  mutate(year = format(as.Date(dt), "%Y"))
+
+build_map <- function(year_choice) {
+  select_year <- temperature_df %>%
+    filter(year == year_choice)
+  
+  calculation <- select_year %>%
+    group_by(Country) %>%
+    filter(dt %in% range(dt, na.rm = TRUE)) %>%
+    summarize(
+      change_in_temp = AverageTemperature[2] - AverageTemperature[1]
+    ) %>%
+    select(Country, change_in_temp) %>%
+    rename(region = Country, Change_in_Temperature = change_in_temp) %>%
+    mutate(
+      region = ifelse(region == "United States", "USA", region),
+      region = ifelse(region == "Congo (Democratic Republic Of The)",
+                      "Democratic Republic of the Congo", region)
+    )
+  
+  
+}
+
+
+
